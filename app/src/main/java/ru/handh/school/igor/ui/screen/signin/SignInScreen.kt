d6c7d2e4e289774d1762a51c4833aef2448751cb
LumakanA <lumakan@yandex.com>
2023-12-10 20:32:10 +0300

    add scroll + reworked offset

--- app/src/main/java/ru/handh/school/igor/ui/screen/signin/SignInScreen.kt
+++ app/src/main/java/ru/handh/school/igor/ui/screen/signin/SignInScreen.kt
@@ -2,10 +2,13 @@ package ru.handh.school.igor.ui.screen.signin
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -13,6 +16,8 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,21 +43,28 @@ private fun SignInContent(
     state: SignInState,
     onAction: (SignInViewAction) -> Unit = {}
 ) {
+    val quarterScreenHeight = with(LocalDensity.current) {
+        (LocalConfiguration.current.screenHeightDp.dp / 8)
+    }
     Scaffold { containerPadding ->
         Box(
             modifier = Modifier
+                .fillMaxSize()
+                .offset(y = quarterScreenHeight)
                 .padding(containerPadding)
-                .padding(AppTheme.offsets.medium)
-                .offset(y = 100.dp),
+                .padding(AppTheme.offsets.medium),
             contentAlignment = Alignment.TopCenter
         ) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = AppTheme.offsets.medium)
+                    .verticalScroll(rememberScrollState()) /* работает не так как надо
+                     * элементы уходят за невидимую границу
+                     * последний элемент обрезается */
             ) {
                 BasicText(
-                    modifier = Modifier.padding(bottom = AppTheme.offsets.large),
+                    modifier = Modifier.padding(bottom = AppTheme.offsets.huge),
                     text = stringResource(R.string.login),
                     style = AppTheme.textStyles.text2
                 )
@@ -60,7 +72,7 @@ private fun SignInContent(
                 AppTextField(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(bottom = AppTheme.offsets.tiny),
+                        .padding(bottom = AppTheme.offsets.large),
                     hint = stringResource(R.string.email),
                     value = state.username,
                     onValueChange = {
@@ -70,14 +82,14 @@ private fun SignInContent(
 
                 AppButton(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = AppTheme.offsets.medium),
+                        .fillMaxWidth(),
                     label = stringResource(R.string.loginButton),
                     loading = state.signInLoading,
                     onClick = {
                         onAction(SignInViewAction.SubmitClicked)
                     }
                 )
+
             }
         }
     }
